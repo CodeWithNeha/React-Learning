1afcd775b7432807887abecab27b3a5b4805f118
Neha <codewithneha2020@gmail.com>
2022-01-10 20:47:26 +0530

    Basic jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
+import log from './Neha.jpeg';
 import './App.css';
-
+import { nativeTouchData } from 'react-dom/test-utils';
+let name = "Neha"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <nav>
+     <li>Home</li>
+     <li>About</li>
+     <li>Contact</li>
+     </nav> 
+     <div className="container">
+       <h1>Hello {name}</h1>
+       <img src={log}/>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet unde molestias veritatis tempora consequatur est quos commodi adipisci laudantium dolores minus, nemo assumenda saepe? Molestias rerum qui deserunt ea, ipsam ratione veritatis dolores modi!</p>
+     </div>
+    </>
   );
 }
 

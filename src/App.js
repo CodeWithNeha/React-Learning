58a222e553292ea707030d46e21f0a45a5e8c371
Neha <codewithneha2020@gmail.com>
2022-01-11 14:16:05 +0530

    Props and PropTypes

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@ import logo from './logo.svg';
 import log from './Neha.jpeg';
 import './App.css';
 import { nativeTouchData } from 'react-dom/test-utils';
+import Navbar from './components/Navbar';
 let name = "Neha"
 function App() {
   return (
     <>
-   <nav>
-     <li>Home</li>
-     <li>About</li>
-     <li>Contact</li>
-     </nav> 
-     <div className="container">
-       <h1>Hello {name}</h1>
-       <img src={log}/>
-       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet unde molestias veritatis tempora consequatur est quos commodi adipisci laudantium dolores minus, nemo assumenda saepe? Molestias rerum qui deserunt ea, ipsam ratione veritatis dolores modi!</p>
-     </div>
+    {/* <Navbar />
+    <Navbar title= "TextUtils"  aboutText="About us"/> */}
+<Navbar title= "TextUtils"/>
     </>
   );
 }

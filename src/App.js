d4bcb0365b73e090ef316fe12043446d8b674f72
Neha <codewithneha2020@gmail.com>
2022-01-13 15:47:34 +0530

    React Router Dom

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import About from './About';
+import About from './components/About';
 import React,{useState} from 'react';
 import Alert from './components/Alert';
-let name = "Neha"
+import {BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   const [mode, setMode] = useState("light")
   const [alert, setalert] = useState(null)
@@ -24,24 +29,33 @@ function App() {
       setMode('dark')
       document.body.style.background = "#042743";
       showAlert("Dark mode has been enabled","success")
+      document.title = 'TextUtils - Dark Mode';
     }
     else{
       setMode('light')
       document.body.style.background = "white";
       showAlert("Light mode has been enabled","success")
+      document.title = 'TextUtils - Light Mode';
     }
   }
   return (
     <>
     {/* <Navbar />
     <Navbar title= "TextUtils"  aboutText="About us"/> */}
+ <Router>
 <Navbar title= "TextUtils" mode={mode} toggleMode={toggleMode}/>
 <Alert alert={alert}></Alert>
 <div className="container my-3">
-<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
-{/* <About></About> */}
+<Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/">
+  <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
+  </Route>
+  </Switch>
 </div>
-
+</Router>
     </>
   );
 }
